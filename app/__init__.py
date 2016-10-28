d54d1ec550ba7261bc98239c4db7f09db9b9984a
Charles Herrera <charles@bassitone.com>
2016-10-27 20:24:49 -0500

    Begin integrating Bootstrap for the frontend

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,13 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
+# from .frontend import frontend
+# from .nav import nav
 
 app = Flask(__name__)
 #app.config.from_object('config')
 from app import views
+Bootstrap(app)
+# app.register_blueprint(frontend)
+app.config['BOOTSTRAP_SERVE_LOCAL'] = True
+
+# nav.init_app(app)
